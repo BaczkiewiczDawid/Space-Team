65f860a7f8a0e715710d8790d18a30c20ff43a90
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-05-25 19:27:02 +0200

    desktop navigation

---
+++ src/components/Logout.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const LogoutBtn = styled.button`
+    display: flex;
+    align-items: center;
+    background-color: transparent;
+    border: none;
+    margin-top: 2rem;
+    margin-left: 1rem;
+    
+    img {
+        width: 1.5rem;
+        height: auto;
+    }
+
+    p {
+        color :${({ theme }) => theme.colors.error};
+        margin-left: 1rem;
+        font-size: 1.2rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      position: fixed;
+      bottom: 0;
+    }
+`;
