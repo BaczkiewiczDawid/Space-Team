ef780774acf8ce6aea0b202f24b2d17aaf14905d
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-05-29 19:59:00 +0200

    adding new post

--- src/components/Dashboard/NewPost.js
+++ src/components/Dashboard/NewPost.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Input } from "components/Dashboard/NewPost.style";
+import Axios from 'axios';
 
 const NewPost = ({ loggedUser }) => {
   const [postDescription, setPostDescription] = useState("");
@@ -8,9 +9,21 @@ const NewPost = ({ loggedUser }) => {
     setPostDescription(e.target.value);
   };
 
-  const handleKeyDown = (e) => {
+  const handlePublicPost = (e) => {
     if (e.key === "Enter" && e.target.value !== "") {
       e.target.value = "";
+
+      const newPost = {
+        author: loggedUser,
+        description: postDescription,
+        img: 'https://images.unsplash.com/photo-1653624824808-e1597cf459a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1033&q=80'
+      }
+
+      Axios.post('http://localhost:5000/api/new-post', {
+        newPost: newPost
+      }).then(() => {
+        console.log('new post added');
+      });
     }
   };
 
@@ -18,7 +31,7 @@ const NewPost = ({ loggedUser }) => {
     <Input
       type="text"
       placeholder="What's on Your mind?"
-      onKeyDown={handleKeyDown}
+      onKeyDown={handlePublicPost}
       onChange={handleSetDescription}
     />
   );
