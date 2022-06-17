3b41312e9f35ca0b4ec904e546d530dd9e484501
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-06-17 19:42:57 +0200

    logo color fix

--- src/components/Dashboard/Logo.js
+++ src/components/Dashboard/Logo.js
@@ -3,7 +3,9 @@ import logo from "assets/images/logo.svg";
 import whiteLogo from 'assets/images/logo-white.svg'
 
 const Logo = ({ theme }) => {
-  return <StyledLogo src={theme === 'light' ? logo : whiteLogo} alt='space-team' />;
+  const logoImg = theme === 'light' ? logo : whiteLogo;
+
+  return <StyledLogo src={logoImg} alt='space-team' />;
 };
 
 export default Logo;
